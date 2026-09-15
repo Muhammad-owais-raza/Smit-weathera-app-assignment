4777dd92426edc0db76f35a301b1b6204c82c25e
Muhammad-owais-raza <owaisraza.codes@gmail.com>
2026-09-16 02:04:45 +0500

    I’m currently working on my weather app and adding new features.

--- ecmaScript.js
+++ ecmaScript.js
@@ -1,22 +1,51 @@
 
+const inputPlace = document.querySelector('#inputPlace');
+const showInputValue = document.querySelector('#show-input-value');
 const showWeather = document.querySelector('#show-weather');
+const showFeelslike = document.querySelector('#show-feelslike'); 
+const showRainChans = document.querySelector('#show-rain-chans');
 const showHumidity = document.querySelector('#show-humidity');
+
+const showWindSpeed = document.querySelector('#show-WindSpeed');
+const showUvIndex = document.querySelector('#show-UV-Index');
+const showPressure = document.querySelector('#show-Pressure');
+const showVisibility = document.querySelector('#show-Visibility');
 
 
 
 async function checkWeather(event) {
     event.preventDefault();
 
-    const inputPlace = document.querySelector('#inputPlace').value;
+    const inputValue = inputPlace.value;
+
+    localStorage.removeItem("Country/City");
+    localStorage.setItem("Country/City", inputValue);
+
     
     const getApi_response = await axios(
-        `https://api.weatherapi.com/v1/current.json?key=e0325e5772cf4dca8f6180742261509&q=${inputPlace}`
+        `https://api.weatherapi.com/v1/current.json?key=e0325e5772cf4dca8f6180742261509&q=${inputValue}`
     );
     
-
-    showWeather.innerHTML = `${inputPlace} Temperacher : ${getApi_response.data.current.temp_c}`;
-
+    console.log(getApi_response);
     
 
+    showInputValue.innerHTML = inputValue;
+    showWeather.innerHTML = 'Temperacher : ' + getApi_response.data.current.temp_c + '°C';
+    showFeelslike.innerHTML = 'Temp Feelslike : ' + getApi_response.data.current.feelslike_c + '°C';
+    showRainChans.innerHTML = 'Chans of rain : ' + getApi_response.data.current.chance_of_rain + '%';
+    showHumidity.innerHTML = 'Humidity : ' + getApi_response.data.current.humidity + '%';
+
+    showWindSpeed.innerHTML = 'WindSpeed : ' + getApi_response.data.current.humidity + ' km/h';
+    showUvIndex.innerHTML = 'UvIndex : ' + getApi_response.data.current.humidity;
+    showPressure.innerHTML = 'Pressure : ' + getApi_response.data.current.humidity + ' mb';
+    showVisibility.innerHTML = 'Visibility : ' + getApi_response.data.current.humidity + ' km';
+
+    
 }
 
+
+const savedCity = localStorage.getItem("Country/City");
+
+if (savedCity) {
+    showInputValue.innerHTML = savedCity;
+}
